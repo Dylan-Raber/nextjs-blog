a6c1afb2c44797a7a4ba0f22b65aad595972cebe
Dylan Raber <dylan.raber02@gmail.com>
2024-10-31 20:42:53 -0600

    Added counter component

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
       <h1 className={styles.title}>
         Dylan Raber welcomes <a href="https://nextjs.org">Next.js!</a>
       </h1>
+      <Counter />
       
     </div>
   );
